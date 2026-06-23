8a75b5b5c389322d007487478e5d0800f13b3f58
torbjorc <torbjorc@gmail.com>
2026-06-23 14:24:17 +0200

    simplify prod.py: use hardcoded ALLOWED_HOSTS and dj_database_url.parse

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,34 +1,24 @@
 import os
-
 import dj_database_url
-from django.core.exceptions import ImproperlyConfigured
-
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = [host for host in os.environ.get('ALLOWED_HOSTS', '*').split(',') if host]
 
-# Database configuration
-if os.getenv('DATABASE_URL'):
-	DATABASES = {
-		'default': dj_database_url.config(
-			conn_max_age=600,
-			ssl_require=True,
-		)
-	}
+ALLOWED_HOSTS = [
+    "carlsen-website.onrender.com",
+    ".onrender.com",
+]
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
 else:
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.sqlite3',
-			'NAME': BASE_DIR / 'db.sqlite3',
-		}
-	}
-
-# Relaxed security for hobby deployment - can enable in production
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_HSTS_SECONDS = int(os.environ.get('SECURE_HSTS_SECONDS', '31536000'))
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
